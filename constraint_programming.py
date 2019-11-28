26b6d373d38ce3d1d8a0742aac9523ec9ee252e0
Christoph Dürr <xtof.durr@gmail.com>
2019-11-28 10:06:46 +0100

    AC3

--- constraint_programming.py
+++ constraint_programming.py
@@ -11,8 +11,14 @@ class constraint_programming:
 
     def add_constraint(self, x, y, rel):
         self.conflict[x].append((y, rel))
-        inv = {(v,u) for (u,v) in  rel}
+        inv = {(v,u) for (u,v) in rel}
         self.conflict[y].append((x, inv))
+
+    def add_constraint_implicit(self, x, y, predicat):
+        rel = {(u,v) for u in self.var[x] 
+                     for v in self.var[y] 
+                     if predicat(u,v)}
+        self.add_constraint(x, y, rel)
 
     def select_var(self):
         x = None
@@ -45,6 +51,64 @@ class constraint_programming:
 
     def restore_context(self):
         self.var = self.context.pop()
+
+
+    def has_support(self, x, u, y, rel):
+        for v in self.var[y]:
+            if (u, v) in rel:
+                return True
+        return False
+
+    def revise(self, x, y, rel):
+        """ le domaine de y vient de diminuer
+        faut-il diminuer le domaine de x à son tour ?
+        """
+        to_remove = set()
+        for u in self.var[x]:
+            if not self.has_support(x, u, y, rel):
+                to_remove.add(u)
+        if to_remove:
+            self.var[x] -= to_remove
+            return True
+        else:
+            return False
+
+    def AC3(self, Q):
+        while Q:
+            y = Q.pop()
+            for x, rel in self.conflict[y]:
+                if self.assign[x] is not None:
+                    if self.revise(x, y, rel):
+                        Q.add(x)
+
+
+    def solve_w_arconsistency(self):
+        self.AC3({x for x in self.var})
+        return self.solve_w_arconsistency_recurse()
+
+    def solve_w_arconsistency_recurse(self):
+        x = self.select_var()
+        if x is None:
+            dump_tree.close()
+            return self.assign
+        domaine = self.var[x]
+        for u in domaine:
+            self.save_context()
+            self.assign[x] = u
+            self.var[x] = {u}
+            self.AC3({x})
+            dump_tree.enter("{}:={}".format(x, u))
+            sol = self.solve_w_arconsistency_recurse()
+            dump_tree.leave()
+            if sol:
+                return sol
+            self.restore_context()
+            self.assign[x] = None
+            self.var[x] = domaine
+        return None
+
+
+
 
 
     def solve(self):
